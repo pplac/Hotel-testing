b03b9fb92d8f1fe6a815168fcf301accdd092f41
Paulina <plac.paulina@gmail.com>
2023-03-31 00:37:22 +0200

    próba buildera1

--- src/main/java/pl/testing/hotel/model/entity/Booking.java
+++ src/main/java/pl/testing/hotel/model/entity/Booking.java
@@ -8,16 +8,16 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Date;
 
-@Getter
-@Setter
-@NoArgsConstructor //konstruktor bez parametrów
-@AllArgsConstructor  //konstruktor z 1 parametrem dla każdego pola
+//@Getter
+//@Setter
+//@NoArgsConstructor //konstruktor bez parametrów
+//@AllArgsConstructor  //konstruktor z 1 parametrem dla każdego pola
 @Entity
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(name = "booking_name")
     private String bookingName;
@@ -41,8 +41,13 @@ public class Booking {
     private Date bookingStop;
 
 
+    public static BookingBuilder builder() {
+        return new BookingBuilder();
+    }
+
     public static class BookingBuilder {
 
+        private long id;
         private String bookingName;
         private String hotelName;
         private int numberOfPeople;
@@ -53,8 +58,42 @@ public class Booking {
 
 
 
-        public BookingBuilder set
+        public BookingBuilder setBookingName(final String bookingName) {
+            this.bookingName = bookingName;
+            return this;
+        }
 
+        public BookingBuilder setHotelName (final String hotelName) {
+            this.hotelName = hotelName;
+            return this;
+        }
+
+        public BookingBuilder setNumberOfPeople(final int numberOfPeople) {
+            this.numberOfPeople = numberOfPeople;
+            return this;
+        }
+
+        public BookingBuilder setRoomStandard(final RoomStandard roomStandard) {
+            this.roomStandard = roomStandard;
+            return this;
+        }
+
+        public BookingBuilder setPrice(final Double price) {
+            this.price = price;
+            return this;
+        }
+
+        public BookingBuilder setBookingStart(final Date bookingStart) {
+            this.bookingStart = bookingStart;
+            return this;
+        }
+
+        public BookingBuilder setBookingStop(final Date bookingStop) {
+            this.bookingStop = bookingStop;
+            return this;
+        }
+
+        public
 
     }
 
